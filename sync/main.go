b92a3dcddbea8edabf8e2e659dcefbd54bdafc81
nongboy.kathawut <princeboy__@hotmail.com>
2021-11-10 17:53:58 +0700

    try to connect database

--- sync/main.go
+++ sync/main.go
@@ -2,20 +2,43 @@ package main
 
 import (
 	"fmt"
-	"myservice/sync/appconf"
-	"myservice/sync/database"
+	"os"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
+
+func connectMySQL() *gorm.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&&parseTime=true",
+		os.Getenv("MYSQL_USERNAME"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_DATABASE"),
+	)
+
+	dbConn, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+		Logger:                                   logger.Default.LogMode(logger.Info),
+		NowFunc: func() time.Time {
+			return time.Now().UTC()
+		},
+	})
+	if err != nil {
+		panic(`fatal error: cannot connect to database`)
+	}
+
+	return dbConn
+}
 
 // Handler func
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	// Setup
-	config := appconf.Read()
-	fmt.Printf("\n config = %+v \n\n", config)
-
-	mySQLConn := database.ConnectMySQL(config.Mysql)
+	mySQLConn := connectMySQL()
 	sqlDB, err := mySQLConn.DB()
 	if err != nil {
 		panic(err)

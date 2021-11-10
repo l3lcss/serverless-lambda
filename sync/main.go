1bff6eded1b7c8dd819004ba0ed3c17bc7acf9c6
nongboy.kathawut <princeboy__@hotmail.com>
2021-11-10 17:21:48 +0700

    try to connect db

--- sync/main.go
+++ sync/main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"myservice/sync/appconf"
+	"myservice/sync/database"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -11,17 +12,17 @@ import (
 // Handler func
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	// Setup
-	// config := appconf.Read()
-	// mySQLConn := database.ConnectMySQL(config.Mysql)
-	// err := mySQLConn.Migrator().DropTable(models.Brand{}, models.BrandLanguage{}, models.Category{}, models.CategoryLanguage{}, models.Product{}, models.ProductLanguage{}, models.Sku{}, models.SkuLanguage{}, models.SellingPriceArea{}, models.SellingPrivatePrice{}, models.Image{}, models.Unit{}, models.UnitLanguage{})
-	// if err != nil {
-	// 	panic("DropTable failed.")
-	// }
+	config := appconf.Read()
+	fmt.Printf("\n config = %+v \n\n", config)
 
-	envVariable1 := os.Getenv("variable1")
-	envGOOS := os.Getenv("GOOS")
-	fmt.Println("envenvVariable1: ", envVariable1)
-	fmt.Println("envGOOS: ", envGOOS)
+	mySQLConn := database.ConnectMySQL(config.Mysql)
+	sqlDB, err := mySQLConn.DB()
+	if err != nil {
+		panic(err)
+	}
+	defer sqlDB.Close()
+
+	fmt.Println("mySQLConn: ", mySQLConn)
 	fmt.Println("Received body: ", request.Body)
 
 	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
